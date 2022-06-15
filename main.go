9a8ebf6464f6a4b82ee3bd9d72ad077c0f78f88e
Ev <dmitry_dtv@mail.ru>
2022-06-15 20:56:26 +0500

    220615

--- main.go
+++ main.go
@@ -2,34 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
 )
 
 func main() {
-	fmt.Println("введите что-нибудь")
-	var Pass string
-	fmt.Scanf("%s\n", &Pass)
-	fmt.Println("вы ввели:", Pass)
+	a := 1.01
 
-	client := http.Client{
-		Timeout: time.Duration(6) * time.Second,
-	}
-	resp, err := client.Get("https://google.com")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-	// io.Copy(os.Stdout, resp.Body)
+	b := 0.99
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	// bodyString := string(body)
-
-	fmt.Printf("\nAnswer for request : %s\t", body)
+	fmt.Println(a - b)
 
 }
