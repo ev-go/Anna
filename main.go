e803d0f507fe715028d04fcadc3eb4c47936826e
ev <dmitry_dtv@mail.ru>
2022-06-15 23:12:15 +0500

    220615

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	a := 1.01
-
-	b := 0.99
-
-	fmt.Println(a - b)
-
+	var a int
+	fmt.Scan(&a)
+	h := a / 30
+	m := a*2 - (h * 60)
+	fmt.Println("it is", h, "hours", m, "minutes.")
 }
