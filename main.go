142b91b100d8d95ab6039433d3d200f5b20551f7
Ev <dmitry_dtv@mail.ru>
2022-06-16 20:48:16 +0500

    220616_1 commit from goland2

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 	var a int
-	ScanCount, err := fmt.Scan(&a)
+	var scanCount, err = fmt.Scan(&a)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(ScanCount)
+	fmt.Println(scanCount)
 	h := a / 30
 	m := a*2 - (h * 60)
 	fmt.Println("it is", h, "hours", m, "minutes.")
