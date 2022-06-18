1692ec739cc0ee8256a30b03d76b3bf8cdc7ab40
ev <dmitry_dtv@mail.ru>
2022-06-18 15:39:28 +0500

    220618

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Vlad!")
+	var a int
+	var scanCount, err = fmt.Scan(&a)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(scanCount)
+	h := a / 30
+	m := a*2 - (h * 60)
+	fmt.Println("it is", h, "hours", m, "minutes.")
 }
